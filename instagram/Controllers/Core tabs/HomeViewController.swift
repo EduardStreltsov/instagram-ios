4e887ec480038b318faef53830d8f7a869c8bbd5
Eduard Streltsov <streltsov.eduard@gmail.com>
2020-09-29 19:58:39 +0300

    Initial commit

---
+++ instagram/Controllers/Core tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by Eduard Streltsov on 28.09.2020.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
